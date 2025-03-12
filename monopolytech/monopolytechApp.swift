ea84e9f7d1b8004b77c00c13290a94f01cb7c324
Eugenio-BAYE <baye.eugenio.egnb+polytech@gmail.com>
2025-03-12 11:18:17 +0100

    fix : add config files for Xcode

---
+++ monopolytech/monopolytechApp.swift
@@ -0,0 +1,17 @@
+//
+//  monopolytechApp.swift
+//  monopolytech
+//
+//  Created by eugenio on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct monopolytechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
